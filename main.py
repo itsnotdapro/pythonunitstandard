44c1da462cd19a47e5cca04d7a58bed89e65e7e6
itsnotdapro <itsnotdapro@users.noreply.github.com>
2018-05-01 07:48:34 +1200

    bunch of shit idk

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
+# Hairdressing Application logic and window function
+# main.py
+# 14/04/18
+
 from windows import ApplicationWindow, ClientList, DeleteClient, Invoice
 import json
 from datetime import datetime
 from tkinter import Tk
 
+# set costs of services, can be changed by editing these values
 cost = { "drive": 0.25,
          "hair": 1.50,
          "mani": 0.75}
 
+# get time in minutes ONLY
 def time_to_minutes(hour, minute, second):
     if hour == "": hour = 0
     if minute == "": minute = 0
     if second == "": second = 0
     return round(int(hour)*60 + int(minute) + int(second)/60)
 
+## Child class of ApplicationWindow, with all functionality added ## 
 class HairdressingApp(ApplicationWindow):
     def __init__(self, master):
         super().__init__(master)
-        self.add_client.config(command=self.add_client_func)
+        now = datetime.now()
+        self.add_client.config(command=self.add_client_func) # set function of the set client button
+        with open('data.json') as file:
+            data = json.load(file) # get data from json file, don't ask why it's json
+            self.set_totals(data, "{}/{}/{}".format(now.day, now.month, now.year)) # set the totals text on lauch
 
+    # get all totals from inputs
     def get_totals(self):
-        rv = {"drive": 0, "hair": 0, "mani": 0}
+        rv = {"drive": 0, "hair": 0, "mani": 0} #rv = return value
         with open("data.json") as file:
-            data = json.load(file)
+            data = json.load(file) #get data from json file
             now = datetime.now()
             current_date = "{}/{}/{}".format(now.day, now.month, now.year)
-            for client in data[current_date]:
+            for client in data[current_date]: # for each value under the current date, add to corresponding index in rv
                 rv["drive"] += data[current_date][client]["drive"]
                 rv["hair"] += data[current_date][client]["hair"]
                 rv["mani"] += data[current_date][client]["mani"]
         return rv
 
+    # set the totals text
     def set_totals(self, data, date):
-        try: type(data[date])
+        try: type(data[date]) # see if any data is under the current date
         except KeyError: return
+
+        # set ALL THE TEXT
         self.total_drive_time.config(text=str(self.get_totals()["drive"]) + " Minutes")
-        self.total_hair_time.config(text=str(self.get_to tals()["hair"]) + " Minutes")
+        self.total_hair_time.config(text=str(self.get_totals()["hair"]) + " Minutes")
         self.total_mani_time.config(text=str(self.get_totals()["mani"]) + " Minutes")
 
         self.total_drive_price.config(text="$" + str(float(self.get_totals()["drive"])*cost["drive"]))
@@ -44,40 +59,43 @@ class HairdressingApp(ApplicationWindow):
         self.total_price.config(text="$" + str(float(self.get_totals()["drive"])*cost["drive"] + \
                                                float(self.get_totals()["hair"])*cost["hair"] + \
                                                float(self.get_totals()["mani"])*cost["mani"]))
-        
+
+    # add client to database, and generate invoice
     def add_client_func(self):
         with open('data.json') as file:
             now = datetime.now()
             new_data = {}
-            data = json.load(file)
+            data = json.load(file) # get data from json file
 
             current_date = "{}/{}/{}".format(now.day, now.month, now.year)
-            try: type(data[current_date])
-            except KeyError: data[current_date] = {}
+            try: type(data[current_date]) # check if any data exists under current date
+            except KeyError: data[current_date] = {} # if not, create a dict under current date
             data[current_date][self.name_input.get()] = {}
             data[current_date][self.name_input.get()]["address"] = self.address_input.get()
             data[current_date][self.name_input.get()]["drive"] = time_to_minutes(self.drive_hours.get(), self.drive_mins.get(), 0)
             data[current_date][self.name_input.get()]["hair"] = time_to_minutes(self.hair_hours.get(), self.hair_mins.get(), 0)
             data[current_date][self.name_input.get()]["mani"] = time_to_minutes(self.mani_hours.get(), self.mani_mins.get(), 0)
 
-        with open('data.json', 'w') as file: json.dump(data, file)
+        with open('data.json', 'w') as file: json.dump(data, file) # write to file
 
-        with open("data.json") as file:
+        with open("data.json") as file: # set the totals, has to be done here as the data needs to be in the json file to be read
             data = json.load(file)
             self.set_totals(data, current_date)
 
-        invoice = Invoice(self, "name", "address", "drive", "hair", "mani", "price")
-        invoice.geometry("300x140")
+        # generate invoice
+        invoice = Invoice(self, self.name_input.get(),
+                                self.address_input.get(),
+                                time_to_minutes(self.drive_hours.get(), self.drive_mins.get(), 0),
+                                time_to_minutes(self.hair_hours.get(), self.hair_mins.get(), 0),
+                                time_to_minutes(self.mani_hours.get(), self.mani_mins.get(), 0), cost)
+        invoice.title(self.name_input.get() + " Invoice")
+        invoice.geometry("325x150")
         invoice.mainloop()
 
-        
+if __name__ == "__main__":        
+    root = Tk()
+    root.geometry("400x600")
+    root.title("Hairdressing Application")
 
-
-
-
-
-root = Tk()
-root.geometry("400x600")
-
-app = HairdressingApp(root)
-root.mainloop()
+    app = HairdressingApp(root)
+    root.mainloop()
--- windows.py
+++ windows.py
@@ -7,6 +7,7 @@ class ApplicationWindow(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
+        # Start creating all the fucking widgets. This is why tkinter is the fucking worst #
         menubar = tk.Menu()
         filebar = tk.Menu(menubar, tearoff=0)
         filebar.add_command(label="Show Today's Clients", command=self.open_client_list)
@@ -72,6 +73,8 @@ class ApplicationWindow(tk.Frame):
         self.total_price = tk.Label(text="$0")
         self.total_price.pack()
 
+        # Stop creating all the goddamn widgets #
+        
     def create_time_inputs(self, text, column, row):
         tk.Label(self.input_grid, text=text).grid(row=row, column=column)
         rv = tk.Entry(self.input_grid, width=7)
@@ -89,8 +92,8 @@ class ApplicationWindow(tk.Frame):
     def open_delete_clients(self):
         now = datetime.now()
         delete = DeleteClient(self, now.day, now.month, now.year)
-        delete.geometry("200x400")
-
+        delete.title("{}/{}/{} Clients".format(now.day, now.month, now.year))
+        delete.geometry("260x400")
         delete.mainloop()
 
 class DeleteClient(tk.Toplevel):
@@ -111,6 +114,7 @@ class DeleteClient(tk.Toplevel):
             with open("data.json") as file:
                 data = json.load(file)
                 current_date = "{}/{}/{}".format(day, month, year)
+                if data[current_date] == {}: raise KeyError
                 for client in data[current_date]:
                     self.buttons[client + "name"] = tk.Label(canframe, text=client, wraplengt=150)
                     self.buttons[client + "name"].pack()
@@ -151,6 +155,7 @@ class ClientList(tk.Toplevel):
             with open("data.json") as file:
                 data = json.load(file)
                 current_date = "{}/{}/{}".format(day, month, year)
+                if data[current_date] == {}: raise KeyError
                 for client in data[current_date]:
                     self.data_list.insert("end", client)
                     self.data_list.insert("end", "Address: " + data[current_date][client]["address"])
@@ -167,13 +172,14 @@ class ClientList(tk.Toplevel):
 
 
 class Invoice(tk.Toplevel):
-    def __init__(self, master, client, address, drive, hair, mani, price):
+    def __init__(self, master, client, address, drive, hair, mani, costs):
         super().__init__(master)
 
-        tk.Label(self, text="Name: " + client).pack(anchor="nw")
-        tk.Label(self, text="Address: " + address).pack(anchor="nw")
-        tk.Label(self, text="Driving Time: " + drive).pack(anchor="nw")
-        tk.Label(self, text="Haircutting Time: " + hair).pack(anchor="nw")
-        tk.Label(self, text="Manicuring Time: " + mani).pack(anchor="nw")
-        tk.Label(self, text="Cost: " + price).pack(anchor="nw")
+        tk.Label(self, text="Name: " + client, wraplengt=325).pack(anchor="nw")
+        tk.Label(self, text="Address: " + address, wraplengt=325).pack(anchor="nw")
+        tk.Label(self, text="Driving Time: " + str(drive), wraplengt=325).pack(anchor="nw")
+        tk.Label(self, text="Haircutting Time: " + str(hair), wraplengt=325).pack(anchor="nw")
+        tk.Label(self, text="Manicuring Time: " + str(mani), wraplengt=325).pack(anchor="nw")
+        price = drive*costs['drive'] + hair*costs['hair'] + mani*costs['mani']
+        tk.Label(self, text="Cost: " + str(price), wraplengt=325).pack(anchor="nw")
         
